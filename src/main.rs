a4f37e57697c808b251168fde56791168bbf7a04
Dowland Aiello <mitsukomegumii@gmail.com>
2020-05-04 12:09:12 -0400

    st-question: Condense into one file.
    Signed-off-by: Dowland Aiello <mitsukomegumii@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,57 @@
+#[macro_use]
+extern crate diesel;
+
 use diesel::{
     mysql::{Mysql, MysqlConnection},
     sql_types::{Bool, Nullable},
     BoxableExpression, Connection, ExpressionMethods, RunQueryDsl,
 };
-use stackoverflow_questions::{
-    models::NewUser,
-    schema::{roles, users},
-};
+use schema::{roles, users};
+use models::{NewUser};
 
 use std::error::Error;
+
+mod schema {
+    table! {
+        roles (user_id) {
+            id -> Unsigned<Bigint>,
+            user_id -> Unsigned<Bigint>,
+            administrator -> Nullable<Bool>,
+            sponsor -> Nullable<Bool>,
+            bot -> Nullable<Bool>,
+        }
+    }
+
+    table! {
+        users (id) {
+            id -> Unsigned<Bigint>,
+            username -> Nullable<Varchar>,
+        }
+    }
+
+    allow_tables_to_appear_in_same_query!(
+        roles,
+        users,
+    );
+}
+
+mod models {
+    use super::schema::users;
+
+    #[derive(Insertable, PartialEq, Debug, Default)]
+    #[table_name = "users"]
+    pub struct NewUser<'a> {
+        username: &'a str,
+    }
+
+    impl<'a> NewUser<'a> {
+        pub fn new(username: &'a str) -> Self {
+            Self {
+                username
+            }
+        }
+    }
+}
 
 enum Role {
     Administrator,
